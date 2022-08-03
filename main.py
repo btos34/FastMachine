2ad2cb35e571c464a1c5e8a5db5f2c4ca9afc16a
Pretzz <88212436+btos34@users.noreply.github.com>
2022-08-03 12:29:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,101 +1,75 @@
+
 import pygame
 import subprocess
 import sys
+import random
 
+RandCaption = random.randint(1,3)
 
 def copy2clip(txt):
     cmd='echo '+txt.strip()+'|clip'
     return subprocess.check_call(cmd, shell=True)
+copy2clip("sarue was here")
 
-copy2clip("wsg")
+
+
+
+## Configeration
+
+## colors
+WHITE = (200,200,200)
+BLACK = (0,0,0)
+GREY = (50,50,50)
+
 pygame.init()
-
-
-
-font = pygame.font.SysFont('Arial', 10)
-
+font = pygame.font.SysFont('Arial', 20)
 objects = []
 
-
-fps = 60
-fpsClock = pygame.time.Clock()
-
-color = (255 ,255,255)
-favicon = pygame.image.load('resources/images/favicon.png')
-
-
-
-pygame.display.set_icon(favicon)
-screen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption("FastMachine: Gotta Go Fast")
+# random title from flash script
+if RandCaption == 1:
+    pygame.display.set_caption("FastMachine: To understand what I'm about to tell you, you need to do something first.")
+if RandCaption == 2:
+    pygame.display.set_caption("FastMachine: You need to believe in the impossible. Can you do that? Good.")
+else:
+    pygame.display.set_caption("FastMachine: You see that red blur? That's me. That too. There I am again.")
 
 
-exit = False
-class Button():
-    
-    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.onclickFunction = onclickFunction
-        self.onePress = onePress
+ 
+pygame.init()
+ 
+ 
 
-        self.fillColors = {
-            'normal': '#ffffff',
-            'hover': '#666666',
-            'pressed': '#333333',
-        }
 
-        self.buttonSurface = pygame.Surface((self.width, self.height))
-        self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+programIcon = pygame.image.load('resources/images/favicon.png')
+screen.fill(GREY)
 
-        self.buttonSurf = font.render(buttonText, True, (20, 20, 20))
+pygame.display.set_icon(programIcon)
 
-        self.alreadyPressed = False
-
-        objects.append(self)
-
-    def process(self):
-
-        mousePos = pygame.mouse.get_pos()
-        
-        self.buttonSurface.fill(self.fillColors['normal'])
-        if self.buttonRect.collidepoint(mousePos):
-            self.buttonSurface.fill(self.fillColors['hover'])
-
-            if pygame.mouse.get_pressed(num_buttons=3)[0]:
-                self.buttonSurface.fill(self.fillColors['pressed'])
-
-                if self.onePress:
-                    self.onclickFunction()
-
-                elif not self.alreadyPressed:
-                    self.onclickFunction()
-                    self.alreadyPressed = True
-
-            else:
-                self.alreadyPressed = False
-
-        self.buttonSurface.blit(self.buttonSurf, [
-            self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
-            self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
-        ])
-        screen.blit(self.buttonSurface, self.buttonRect)
-def VaultBasic():
-    copy2clip("V3;2P;2P;{(0(3n.)h)08}ddd(2O(2D)}}}$})05h(2O(2E)t}}(5u)8(2O(2D)9r}}h(5r)6}(2O(2D)dhv}9(2N)6D(2O(2E)vf}d}7}}}}3(2O(2F)vNd)07N(3Eh(3n=)(0(q{);The Machine Box V1.2;;0")
-    
-customButton = Button(30, 30, 160, 40, 'Copy Basic Vault', VaultBasic)
-
-while True:
-    screen.fill((20, 20, 20))
+def drawGrid():
+    blockSize = 20 #Set the size of the grid block
+    for x in range(0, 700, blockSize):
+        for y in range(0, 500, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(screen, WHITE, rect, 1)
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    drawGrid()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    for object in objects:
-        object.process()
-
+            done = True
     pygame.display.flip()
-    fpsClock.tick(fps)
+    clock.tick(60)
+
+pygame.quit()
+
+
